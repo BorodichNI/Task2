bad9ecc41491e08c02d041260fe4b2d998100a8c
nikolay <Borodich_nikolay@mail.ru>
2021-01-11 14:31:58 +0300

    Initial commit

---
+++ src/main/java/Finder.java
@@ -0,0 +1,11 @@
+public class Finder {
+
+    public static void main(String[] args) {
+        MaxValue maxValue = new MaxValue();
+        maxValue.value();
+        System.out.println(" ");
+        SecondMaxValue second = new SecondMaxValue();
+        second.secondValue();
+    }
+
+}
